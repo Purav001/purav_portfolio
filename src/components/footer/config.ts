d07b0a04bea7feb01c5785b2a1e7ad81d02183b9
Purav001 <puravsaluja4@gmail.com>
2026-02-03 19:25:05 +0530

    Initial commit

---
+++ src/components/footer/config.ts
@@ -0,0 +1,8 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Resume",
+    href: "https://drive.google.com/file/d/1pfzhBQNKqmezyaA2x-bd83PFeHSKozjG/view?usp=sharing",
+  },
+];
+
+export { footer };
---
+++ src/data/config.ts
@@ -0,0 +1,47 @@
+const config = {
+  title: "Purav Saluja | AI Enthusiast",
+  description: {
+    long: "Explore the portfolio of Purav, a Computer Science Engineer with 2 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows & also strong ML concepts. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Purav, a AI enthusiast solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Purav",
+    "Purav Saluja",
+    "puravsaluja",
+    "puravsaluja.com",
+    "Purav Saluja portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "Machine Learning",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Purav Saluja",
+  email: "puravsaluja.dev@gmail.com",
+  site: "https://abhijitzende.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/_Abhi_Speaks/",
+    linkedin: "https://www.linkedin.com/in/puravsaluja4/",
+    instagram: "https://www.instagram.com/puravsaluja13/",
+    github: "https://github.com/Purav001/",
+  },
+};
+export { config };
